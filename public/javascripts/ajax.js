74345ab3bf7d13fbaff133187329945d52edc2f0
khong97 <kevin.zy.hong@gmail.com>
2016-07-29 16:07:58 -0500

    clientside js

---
+++ public/javascripts/ajax.js
@@ -0,0 +1,124 @@
+var dictionary;
+
+function initialize() {
+    //Setup Google Map
+    var myLatlng = new google.maps.LatLng(40.1020, -88.2272);
+    var light_grey_style = [{
+        "featureType": "landscape",
+        "stylers": [{
+            "saturation": -100
+        }, {
+            "lightness": 65
+        }, {
+            "visibility": "on"
+        }]
+    }, {
+        "featureType": "poi",
+        "stylers": [{
+            "saturation": -100
+        }, {
+            "lightness": 51
+        }, {
+            "visibility": "simplified"
+        }]
+    }, {
+        "featureType": "road.highway",
+        "stylers": [{
+            "saturation": -100
+        }, {
+            "visibility": "simplified"
+        }]
+    }, {
+        "featureType": "road.arterial",
+        "stylers": [{
+            "saturation": -100
+        }, {
+            "lightness": 30
+        }, {
+            "visibility": "on"
+        }]
+    }, {
+        "featureType": "road.local",
+        "stylers": [{
+            "saturation": -100
+        }, {
+            "lightness": 40
+        }, {
+            "visibility": "on"
+        }]
+    }, {
+        "featureType": "transit",
+        "stylers": [{
+            "saturation": -100
+        }, {
+            "visibility": "simplified"
+        }]
+    }, {
+        "featureType": "administrative.province",
+        "stylers": [{
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [{
+            "visibility": "on"
+        }, {
+            "lightness": -25
+        }, {
+            "saturation": -100
+        }]
+    }, {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [{
+            "hue": "#ffff00"
+        }, {
+            "lightness": -25
+        }, {
+            "saturation": -97
+        }]
+    }];
+    var myOptions = {
+        zoom: 2,
+        center: myLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: google.maps.ControlPosition.LEFT_BOTTOM
+        }
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+    //Setup heat map and link to Twitter array we will append data to
+    var heatmap;
+    var liveTweets = new google.maps.MVCArray();
+    heatmap = new google.maps.visualization.HeatmapLayer({
+        data: liveTweets,
+        radius: 25
+    });
+    heatmap.setMap(map);
+
+}
+
+function loadXMLDoc() {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+           if (xmlhttp.status == 200) {
+               document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+           }
+           else if (xmlhttp.status == 400) {
+              alert('There was an error 400');
+           }
+           else {
+               alert('something else other than 200 was returned');
+           }
+        }
+    };
+
+    xmlhttp.open("GET", "localhost:8000/ajax", true);
+    xmlhttp.send();
+}
